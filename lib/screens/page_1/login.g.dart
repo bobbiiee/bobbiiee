2bfaabf37c0da8e51177a9885f9afed62492b791
Parabeac-Nest <admin@parabeac.com>
2022-05-03 13:54:52 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/screens/page_1/login.g.dart
@@ -0,0 +1,271 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Login extends StatefulWidget {
+  const Login({
+    Key? key,
+  }) : super(key: key);
+  @override
+  _Login createState() => _Login();
+}
+
+class _Login extends State<Login> {
+  _Login();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Stack(children: [
+        Positioned(
+          left: 0,
+          width: MediaQuery.of(context).size.width * 1.0,
+          top: 0,
+          height: MediaQuery.of(context).size.height * 0.462,
+          child: Center(
+              child: Container(
+                  height: 428.0,
+                  width: 428.0,
+                  child: Image.asset(
+                    'assets/images/rectangle77.png',
+                    package: 'club4care',
+                    width: 428.000,
+                    height: 428.000,
+                    fit: BoxFit.scaleDown,
+                  ))),
+        ),
+        Positioned(
+          left: 39.0,
+          width: 350.0,
+          top: 470.0,
+          height: 111.0,
+          child: Container(
+              width: 350.000,
+              height: 111.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 350.0,
+                  top: 0,
+                  height: 45.0,
+                  child: Container(
+                    width: 350.000,
+                    height: 45.000,
+                    decoration: BoxDecoration(
+                      color: Color(0x66c5baba),
+                      borderRadius: BorderRadius.all(Radius.circular(65)),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 62.27,
+                  width: 81.595,
+                  top: 15.0,
+                  height: 21.0,
+                  child: Container(
+                      width: 81.595,
+                      height: 21.000,
+                      child: AutoSizeText(
+                        'E-mail',
+                        style: TextStyle(
+                          fontFamily: 'Arial',
+                          fontSize: 17.224557876586914,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 1.3779646301269532,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.left,
+                      )),
+                ),
+                Positioned(
+                  left: 21.0,
+                  width: 20.0,
+                  top: 15.0,
+                  height: 20.0,
+                  child: Container(
+                      width: 20.000,
+                      height: 20.000,
+                      child: AutoSizeText(
+                        'email',
+                        style: TextStyle(
+                          fontFamily: 'Material Icons',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+                Positioned(
+                  left: 0,
+                  width: 350.0,
+                  top: 66.0,
+                  height: 45.0,
+                  child: Container(
+                    width: 350.000,
+                    height: 45.000,
+                    decoration: BoxDecoration(
+                      color: Color(0x66c9c8c8),
+                      borderRadius: BorderRadius.all(Radius.circular(65)),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 62.27,
+                  width: 117.025,
+                  top: 77.0,
+                  height: 21.0,
+                  child: Container(
+                      width: 117.025,
+                      height: 21.000,
+                      child: AutoSizeText(
+                        'Password',
+                        style: TextStyle(
+                          fontFamily: 'Arial',
+                          fontSize: 17.224557876586914,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 1.3779646301269532,
+                          color: Color(0xd9000000),
+                        ),
+                        textAlign: TextAlign.left,
+                      )),
+                ),
+                Positioned(
+                  left: 16.104,
+                  width: 30.061,
+                  top: 73.0,
+                  height: 25.0,
+                  child: Image.asset(
+                    'assets/images/heartlock.png',
+                    package: 'club4care',
+                    width: 30.061,
+                    height: 25.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+              ])),
+        ),
+        Positioned(
+          left: 48.0,
+          width: 332.312,
+          top: 607.0,
+          height: 120.0,
+          child: Container(
+              width: 332.312,
+              height: 120.000,
+              decoration: BoxDecoration(),
+              child: Stack(children: [
+                Positioned(
+                  left: 0,
+                  width: 331.671,
+                  top: 21.0,
+                  height: 45.0,
+                  child: Container(
+                    width: 331.671,
+                    height: 45.000,
+                    decoration: BoxDecoration(
+                      color: Color(0x9994befc),
+                      borderRadius: BorderRadius.all(Radius.circular(45)),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  width: 331.671,
+                  top: 76.0,
+                  height: 45.0,
+                  child: Container(
+                    width: 331.671,
+                    height: 45.000,
+                    decoration: BoxDecoration(
+                      color: Color(0x99feffd0),
+                      borderRadius: BorderRadius.all(Radius.circular(45)),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  width: 134.312,
+                  top: 0,
+                  height: 1.0,
+                  child: SvgPicture.asset(
+                    'assets/images/line1.svg',
+                    package: 'club4care',
+                    width: 134.312,
+                    height: 1.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 198.0,
+                  width: 134.312,
+                  top: 0,
+                  height: 1.0,
+                  child: SvgPicture.asset(
+                    'assets/images/line2.svg',
+                    package: 'club4care',
+                    width: 134.312,
+                    height: 1.000,
+                    fit: BoxFit.none,
+                  ),
+                ),
+                Positioned(
+                  left: 114.105,
+                  width: 92.583,
+                  top: 33.0,
+                  height: 21.0,
+                  child: Container(
+                      width: 92.583,
+                      height: 21.000,
+                      child: AutoSizeText(
+                        'Log in',
+                        style: TextStyle(
+                          fontFamily: 'Arial',
+                          fontSize: 17.224557876586914,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 1.3779646301269532,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+                Positioned(
+                  left: 118.175,
+                  width: 93.601,
+                  top: 88.0,
+                  height: 10.0,
+                  child: Container(
+                      width: 93.601,
+                      height: 10.000,
+                      child: AutoSizeText(
+                        'Register',
+                        style: TextStyle(
+                          fontFamily: 'Arial',
+                          fontSize: 17.224557876586914,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 1.3779646301269532,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      )),
+                ),
+              ])),
+        ),
+      ]),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
